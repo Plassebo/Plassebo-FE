6dd5a3692fa8369fec6b87983ad838c0c04f9cfe
Eunse Jang <senuej37@gmail.com>
2023-08-03 00:51:31 +0900

    feat: 홈화면 네이버 지도 api 연동

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,10 @@ import 'package:plassebo_flutter/widgets/footer.dart';
 import 'package:plassebo_flutter/widgets/header.dart';
 import 'package:plassebo_flutter/widgets/drawer_menu.dart';
 
+// 네이버 지도 관련
+import 'dart:async';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,11 +22,13 @@ class ContainerScreen extends StatefulWidget {
 class _ContainerScreen extends State<ContainerScreen> {
   int _selectedIndex = 0;
 
+// 네이버 지도
+  Completer<NaverMapController> _controller = Completer();
+  MapType _mapType = MapType.Basic;
+
   final List<Widget> _pages = [
     HomeScreen(),
-    HomeScreen(),
-    Page1Screen(),
-    Page2Screen(),
+    ContainerScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -36,12 +42,22 @@ class _ContainerScreen extends State<ContainerScreen> {
     return Scaffold(
         appBar: Header(),
         drawer: DrawerMenu(),
-        body: _pages[_selectedIndex],
+        body: Container(
+            child: NaverMap(
+          onMapCreated: onMapCreated,
+          mapType: _mapType,
+        )),
+        //body: _pages[_selectedIndex],
         bottomNavigationBar: Footer(
           onItemTapped: _onItemTapped,
           pages: _pages,
           selectedIndex: _selectedIndex,
         ));
+  }
+
+  void onMapCreated(NaverMapController controller) {
+    if (_controller.isCompleted) _controller = Completer();
+    _controller.complete(controller);
   }
 }
 
@@ -51,26 +67,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text('홈 화면'),
-    );
-  }
-}
-
-// 페이지 1 화면
-class Page1Screen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('페이지 1'),
-    );
-  }
-}
-
-// 페이지 2 화면
-class Page2Screen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('페이지 2'),
     );
   }
 }
