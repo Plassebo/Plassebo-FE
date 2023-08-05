f080d1978d15189ea35e14ac5742ea515f7764de
nykoh2001 <nykoh2001@gmail.com>
2023-08-05 18:09:39 +0900

    feat: 채팅 고도화

--- lib/data/provider/rule_chat.dart
+++ lib/data/provider/rule_chat.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:plassebo_flutter/data/provider/request.dart';
@@ -5,6 +7,28 @@ import 'package:plassebo_flutter/data/provider/request.dart';
 String responseText = '';
 String restName = "";
 const String reqeustUrl = "http://34.22.67.47:8080/questions";
+
+String makeResponse(String restaurantName, String type, dynamic info) {
+  switch (type) {
+    case "주소":
+      return "${restaurantName}은(는) ${info}에 위치하고 있습니다.";
+    case "전화번호":
+      return "${restaurantName}의 매장 번호는 ${info}입니다.";
+    case "영업시간":
+      String times = "";
+      for (var t in jsonDecode(info).entries) {
+        times = "$times\n\n${t.key}:\n${t.value}";
+      }
+      return "${restaurantName}의 영업 시간은 아래와 같습니다.${times}";
+    case "가격":
+      dynamic entry = jsonDecode(info).entries.toList()[0];
+      return "${restaurantName}의 대표 메뉴와 그 가격을 알려드릴게요.\n\n${restaurantName}의 대표 메뉴는 ${entry.key}이고, 그 가격은 ${entry.value}입니다.";
+    case "기타":
+      return "${restaurantName}의 추가 정보를 알려드릴게요.\n\n${info}";
+    default:
+      return "올바르지 않은 요청";
+  }
+}
 
 Map<String, List<String>> keywordsDict = {
   "greet": ["안녕", "안녕하세요", "하이", "ㅎㅇ"],
@@ -102,7 +126,7 @@ void getResponse(String inputString) async {
     String resString = await postQuestions("http://34.22.67.47:8080/questions",
         {"restaurantName": restName, "type": type});
     FirebaseFirestore.instance.collection("chat").add({
-      'text': "$restName의 $type 정보입니다.\n\n$restName의 $type는 ${resString}입니다!",
+      'text': makeResponse(restName, type, resString),
       'time': Timestamp.now(),
       'isUser': false
     });
