294b8e4025693662f6037f5204cf2f9aa594b5da
nykoh2001 <nykoh2001@gmail.com>
2023-08-06 14:05:33 +0900

    chore: rebuild

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:plassebo_flutter/screens/login.dart';
 import 'package:plassebo_flutter/screens/home.dart';
 import 'package:plassebo_flutter/screens/nearby.dart';
-import 'package:plassebo_flutter/screens/signUp.dart';
 
 import 'package:plassebo_flutter/screens/chatting.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -31,8 +29,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: MainScreen(),
       routes: {
-        '/login': (context) => Login(),
-        '/signUp': (context) => SignUp(),
         '/home': (context) => Home(),
         '/nearby': (context) => NearBy(isCamera: true),
         '/chatting': (context) => Chatting(),
@@ -57,44 +53,15 @@ class MainScreen extends StatelessWidget {
                 color: Color(0xFF4A7DFF),
               ),
             ),
-            SizedBox(height: 120),
-            SizedBox(
-              width: 125,
-              height: 40,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/signUp');
-                },
-                child: Text('회원가입', style: TextStyle(fontSize: 18)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF4A7DFF),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            SizedBox(
-              width: 125,
-              height: 40,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/login');
-                },
-                child: Text(
-                  '로그인',
-                  style: TextStyle(fontSize: 18),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF9FBAFF),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
             SizedBox(height: 50),
+            Text(
+              '이미지 분류, 룰베이스 챗봇 기반 부산 맛집 정보 관광 앱',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFFDDDDDD),
+              ),
+            ),
             SizedBox(
               width: 160,
               height: 30,
@@ -104,26 +71,6 @@ class MainScreen extends StatelessWidget {
                 },
                 child: Text(
                   '임시 메인 이동 버튼',
-                  style: TextStyle(fontSize: 15),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 94, 94, 94),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            SizedBox(
-              width: 160,
-              height: 30,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/chatting');
-                },
-                child: Text(
-                  '임시 채팅 이동 버튼',
                   style: TextStyle(fontSize: 15),
                 ),
                 style: ElevatedButton.styleFrom(
